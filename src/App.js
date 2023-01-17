82ea1c7114ad8d5c2f0fa7fb9116a51e4e5ada1e
George Treneff <treneff@protonmail.com>
2023-01-17 10:57:26 +0000

    :sparkles: route animations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Header from './components/Header/Header';
 import Main from './containers/Main';
 import Footer from './components/Footer/Footer';
-import { HashRouter as Router } from 'react-router-dom';
+import './Animations.css'
+import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
     return (
         <Router>
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import './Main.css'
 import Home from '../components/Home';
 import About from '../components/About';
 import Skills from '../components/Skills';
 import Work from '../components/Work';
 
 const Main = () => {
+    const location = useLocation();
+    const [displayLocation, setDisplayLocation] = useState(location);
+    const [transitionStage, setTransitionStage] = useState('slideIn');
+
+    useEffect(() => {
+        if (location !== displayLocation) setTransitionStage('slideOut');
+    }, [location, displayLocation]);
+
     return (
-        <main>
-            <Routes>
+        <main
+            className={`${transitionStage}`}
+            onAnimationEnd={() => {
+                if (transitionStage === 'slideOut') {
+                    setTransitionStage('slideIn');
+                    setDisplayLocation(location);
+                }
+            }}>
+            <Routes location={displayLocation}>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/skills' element={<Skills />} />
